b6f8cdc3661cd2b5d2191c41ff311dbf0c4162b1
Ivan Jaramillo <converse_ivan@hotmail.com>
2016-05-23 22:13:25 -0500

    Resolucion

--- piramide-caracteres.rb
+++ piramide-caracteres.rb
@@ -10,14 +10,16 @@
 =end
 
 #Ingresar caracter
-print "Ingresa un caracter"
+print "Ingresa un caracter: "
 c = gets.chomp #c recibe lo que el usuario escriba
 c = c[0] #Solo almacenamos el primero caracter..
 
 print "Ingrese un número: " #Pedimos al usuario el numero
 n = gets.chomp.to_i.abs #Recibimos el numero y lo pasamos a entero y a un numero absoluto
 
+valor = c #Agregamos nueva variable
 #Hacemos un for del rango de 1 hasta el numero para pintar los caracteres
 for i in 1..n
-  puts " " * (n - i) #Al numero le vamos a restar la iteracion
+  puts " " * (n - i) + valor #Al numero le vamos a restar la iteracion para calcular los espacios
+  valor += c * 2 #se suman caracteres en cada iteración
 end
