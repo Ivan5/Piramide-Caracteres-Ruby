acb3b808a558f190db22d873d84f49b507e82174
Ivan Jaramillo <converse_ivan@hotmail.com>
2016-05-23 21:45:52 -0500

    Initial Commit

---
+++ piramide-caracteres.rb
@@ -0,0 +1,10 @@
+=begin
+    Crear un programa en el cual se ingrese por teclado un carácter y un número,
+    a partir de esoos datos ingresados, el programa debe generar una pirámide.
+
+    Ejemplo:
+    Si le enviamos al programa el carácter + y el número 3, el programa debe mostrar lo siguinte:
+        +
+       +++
+      +++++
+=end
